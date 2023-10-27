d9a5de92ae9e17e4a2a090093861ad38fe0dec5a
Grigory Dubrovin <wackywildoak@gmail.com>
2023-10-27 17:10:28 +0900

    tsk10141 test_amur: test modal window added
    Добавил тестовое модальное окно

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
+// всплывающее мобильное меню
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector(".header-mobile").classList.toggle("open")
     })
 })
 
+// всплывающее планшетное меню
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger-tablet").addEventListener("click", function() {
         document.querySelector(".header-tablet").classList.toggle("open")
     })
 })
+
+// модальное окно
+
+let modalWindow = document.getElementById("modal-window");
+
+let modalButton = document.getElementById("modal-btn");
+
+let closeButton = document.getElementsByClassName("close")[0];
+
+modalButton.onclick = function() {
+    modalWindow.style.display = "block";
+}
+
+closeButton.onclick = function() {
+    modalWindow.style.display = "none";
+}
+
