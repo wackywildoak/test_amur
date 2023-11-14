b97733e7af46d450f330c07a29e2e0b668257e37
Grigory Dubrovin <thefiregms@gmail.com>
2023-11-14 17:29:12 +0900

    tsk10141 test_amur: add comments code

--- php/log_in.php
+++ php/log_in.php
@@ -1,14 +1,22 @@
 <?php
+// получение данных с полей формы
 $login = $_POST["login"];
 $password = md5($_POST["password"]);
+
+//сообщение о неправильном пароле
 $wrongPassword = "";
+
+//подключение к базе данных
 $dbc = mysqli_connect('localhost', 'root', '', 'reg');
 
+
+//проверка на пустые поля
 if (!empty($login) && !empty($password)) {
     $query = "SELECT * FROM users_log WHERE log_name = '$login' and log_password = '$password'";
-    $res = mysqli_query($dbc, $query);
-    $user = mysqli_fetch_array($res);
+    $res = mysqli_query($dbc, $query); // отправляем запрос к базе данных
+    $user = mysqli_fetch_array($res); // получаем данные
 
+    // если данные верны
     if (!empty($user)) {
         $_SESSION['auth'] = true;
         $id = mysqli_insert_id($dbc);
@@ -16,11 +24,12 @@ if (!empty($login) && !empty($password)) {
         setcookie('login', $login, time() + 3600 * 24, '/');
         setcookie('authCookie', true, time() + 3600 * 24, '/');
         header("Location: /login.php");
-        
+    // если неверны
     } else {
         $_SESSION['auth'] = false;
-        $wrongPassword = "Неправильный пароль";
+        $wrongPassword = "Неправильный логин или пароль";
     }
+// если поля пустые
 } else {
     $wrongPassword = "Введите логин и пароль";
 }
